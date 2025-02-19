6aabda47909a257c3a778593800afabeca818cdf
aaronberger@hotmail.de <aaronberger@hotmail.de>
2025-02-19 15:57:49 +0100

    included exception for non one 2 any property generators

--- robustness_experiment_box/verification_module/attack_estimation_module.py
+++ robustness_experiment_box/verification_module/attack_estimation_module.py
@@ -6,6 +6,7 @@ import time
 from robustness_experiment_box.database.verification_context import VerificationContext
 from robustness_experiment_box.verification_module.verification_module import VerificationModule
 from robustness_experiment_box.database.verification_result import VerificationResult
+from robustness_experiment_box.verification_module.property_generator.one2any_property_generator import One2AnyPropertyGenerator
 
 from robustness_experiment_box.verification_module.attacks.attack import Attack
 
@@ -17,26 +18,30 @@ class AttackEstimationModule(VerificationModule):
 
     def verify(self, verification_context: VerificationContext, epsilon: float) -> str | CompleteVerificationData:
 
-        start = time.time()
-        
-        torch_model = verification_context.network.load_pytorch_model()
+        if isinstance(verification_context.property_generator, One2AnyPropertyGenerator):
 
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        target = verification_context.data_point.label
-        target_on_device = torch.tensor([target], device=device)
-        data_on_device = verification_context.data_point.data.clone().detach().to(device)
+            start = time.time()
+            
+            torch_model = verification_context.network.load_pytorch_model()
+
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            target = verification_context.data_point.label
+            target_on_device = torch.tensor([target], device=device)
+            data_on_device = verification_context.data_point.data.clone().detach().to(device)
 
 
-        perturbed_data = self.attack.execute(torch_model, data_on_device, target_on_device, epsilon)
+            perturbed_data = self.attack.execute(torch_model, data_on_device, target_on_device, epsilon)
 
-        output = torch_model(perturbed_data)
+            output = torch_model(perturbed_data)
 
-        _, final_pred = output.max(1, keepdim=True)
+            _, final_pred = output.max(1, keepdim=True)
 
-        duration = time.time() - start
+            duration = time.time() - start
 
-        if final_pred == target:
-            return CompleteVerificationData(result=VerificationResult.UNSAT, took=duration)
-        
+            if final_pred == target:
+                return CompleteVerificationData(result=VerificationResult.UNSAT, took=duration)
+            
+            else:
+                return CompleteVerificationData(result=VerificationResult.SAT, took=duration)
         else:
-            return CompleteVerificationData(result=VerificationResult.SAT, took=duration)
+            raise NotImplementedError("Currently, only one 2 any verification is implemented for adversarial attacks.")
