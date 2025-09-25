24a29812d3afe9f9f52877ff1747e2cd06e35d7d
Hendrik <hendrik@hendrikusb.com>
2025-09-25 19:14:48 +0200

    feat: expose missing concrete classes in package API and remove non-extistent functions from __all__

--- __init__.py
+++ __init__.py
@@ -20,19 +20,40 @@ from .robustness_experiment_box import (
 )
 
 with contextlib.suppress(ImportError):
+    # Database classes
+    from .robustness_experiment_box.database.dataset.data_point import DataPoint
+    from .robustness_experiment_box.database.dataset.experiment_dataset import ExperimentDataset
     from .robustness_experiment_box.database.dataset.image_file_dataset import ImageFileDataset
     from .robustness_experiment_box.database.dataset.pytorch_experiment_dataset import PytorchExperimentDataset
+    from .robustness_experiment_box.database.epsilon_status import EpsilonStatus
+    from .robustness_experiment_box.database.epsilon_value_result import EpsilonValueResult
     from .robustness_experiment_box.database.experiment_repository import ExperimentRepository
     from .robustness_experiment_box.database.machine_learning_model.network import Network
+    from .robustness_experiment_box.database.machine_learning_model.onnx_network import ONNXNetwork
+    from .robustness_experiment_box.database.machine_learning_model.pytorch_network import PyTorchNetwork
+    from .robustness_experiment_box.database.machine_learning_model.torch_model_wrapper import TorchModelWrapper
     from .robustness_experiment_box.database.verification_context import VerificationContext
+    from .robustness_experiment_box.database.verification_result import VerificationResult
+    from .robustness_experiment_box.database.vnnlib_property import VNNLibProperty
+
+    # Dataset sampler classes
     from .robustness_experiment_box.dataset_sampler.dataset_sampler import DatasetSampler
     from .robustness_experiment_box.dataset_sampler.predictions_based_sampler import PredictionsBasedSampler
+
+    # Epsilon value estimator classes
     from .robustness_experiment_box.epsilon_value_estimator.binary_search_epsilon_value_estimator import (
         BinarySearchEpsilonValueEstimator,
     )
     from .robustness_experiment_box.epsilon_value_estimator.epsilon_value_estimator import EpsilonValueEstimator
+    from .robustness_experiment_box.epsilon_value_estimator.iterative_epsilon_value_estimator import (
+        IterativeEpsilonValueEstimator,
+    )
+
+    # Verification module classes
     from .robustness_experiment_box.verification_module.attack_estimation_module import AttackEstimationModule
+    from .robustness_experiment_box.verification_module.attacks.attack import Attack
     from .robustness_experiment_box.verification_module.attacks.auto_attack_wrapper import AutoAttackWrapper
+    from .robustness_experiment_box.verification_module.attacks.fgsm_attack import FGSMAttack
     from .robustness_experiment_box.verification_module.attacks.pgd_attack import PGDAttack
     from .robustness_experiment_box.verification_module.auto_verify_module import (
         AutoVerifyModule,
@@ -42,6 +63,10 @@ with contextlib.suppress(ImportError):
     from .robustness_experiment_box.verification_module.property_generator.one2any_property_generator import (
         One2AnyPropertyGenerator,
     )
+    from .robustness_experiment_box.verification_module.property_generator.one2one_property_generator import (
+        One2OnePropertyGenerator,
+    )
+    from .robustness_experiment_box.verification_module.property_generator.property_generator import PropertyGenerator
     from .robustness_experiment_box.verification_module.verification_module import VerificationModule
 
 # Check for autoattack availability
@@ -72,26 +97,46 @@ __all__ = [
     "util",
     "verification_module",
     
+    # Core abstract classes
     "DatasetSampler",
     "EpsilonValueEstimator",
     "VerificationModule",
-   
-    "ExperimentRepository",
     "Network",
+    "PropertyGenerator",
+    "Attack",
+    "ExperimentDataset",
+   
+    # Database classes
+    "ExperimentRepository",
     "VerificationContext",
+    "ONNXNetwork",
+    "PyTorchNetwork",
+    "TorchModelWrapper",
+    "VNNLibProperty",
+    "VerificationResult",
+    "EpsilonValueResult",
+    "EpsilonStatus",
+    "DataPoint",
 
+    # Dataset sampler classes
     "PredictionsBasedSampler",
     "PytorchExperimentDataset",
     "ImageFileDataset",
+
+    # Epsilon value estimator classes
     "BinarySearchEpsilonValueEstimator",
+    "IterativeEpsilonValueEstimator",
+
+    # Verification module classes
     "AttackEstimationModule",
     "PGDAttack",
+    "FGSMAttack",
     "AutoAttackWrapper",
     "AutoVerifyModule",
     "parse_counter_example",
     "parse_counter_example_label",
 
+    # Property generator classes
     "One2AnyPropertyGenerator",
-    "list_auto_verify_verifiers",
-    "create_auto_verify_verifier",
+    "One2OnePropertyGenerator",
 ]
