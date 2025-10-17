766fa41ad1e610d618f48c21779eabd52a21c0bc
AWbosman <annelotbosman@gmail.com>
2025-10-17 17:41:29 +0200

    added new tests to cover verbose line

--- tests/test_verification_module/attacks/test_auto_attack_wrapper.py
+++ tests/test_verification_module/attacks/test_auto_attack_wrapper.py
@@ -1,6 +1,37 @@
+import pytest
+from pyautoattack import AutoAttack
 
 
+class DummyModel:
+    def __call__(self, x):
+        return x
 
+@pytest.fixture
+def dummy_model():
+    return DummyModel()
+
+def test_verbose_assignment(dummy_model):
+    epsilon = 0.1
+    norm = 'Linf'
+    version = 'standard'
+    verbose_value = False  # test with True/False
+
+    adversary = AutoAttack(
+        dummy_model,
+        norm=norm,
+        eps=epsilon,
+        version=version,
+        device='cpu',
+    )
+
+    # assign verbose
+    adversary.verbose = verbose_value
+
+    # test that it was correctly set
+    assert hasattr(adversary, 'verbose'), "AutoAttack should have a 'verbose' attribute"
+    assert adversary.verbose == verbose_value, "Verbose flag not correctly set"
+    
+    
 def test_auto_attack_wrapper_initialization(attack_wrapper):
     assert attack_wrapper.device == 'cpu'
     assert attack_wrapper.norm == 'Linf'
