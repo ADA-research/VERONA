e8bc3ba69a18f872c18542c573c21aacdfc8e42f
AWbosman <annelotbosman@gmail.com>
2025-10-17 18:01:56 +0200

    extended autoattack tests

--- tests/test_verification_module/attacks/test_auto_attack_wrapper.py
+++ tests/test_verification_module/attacks/test_auto_attack_wrapper.py
@@ -1,37 +1,23 @@
 import pytest
-from pyautoattack import AutoAttack
+import torch
 
 
-class DummyModel:
-    def __call__(self, x):
-        return x
+@pytest.mark.parametrize("verbose_value", [True, False])
+def test_autoattack_verbose_assignment(attack_wrapper, model,data,target,verbose_value):
 
-@pytest.fixture
-def dummy_model():
-    return DummyModel()
-
-def test_verbose_assignment(dummy_model):
-    epsilon = 0.1
-    norm = 'Linf'
-    version = 'standard'
-    verbose_value = False 
-
-    adversary = AutoAttack(
-        dummy_model,
-        norm=norm,
-        eps=epsilon,
-        version=version,
-        device='cpu',
-    )
-
-    adversary.verbose = verbose_value
-
-
-    assert hasattr(adversary, 'verbose'), "AutoAttack should have a 'verbose' attribute"
-    assert adversary.verbose == verbose_value, "Verbose flag not correctly set"
-    
-    
-def test_auto_attack_wrapper_initialization(attack_wrapper):
     assert attack_wrapper.device == 'cpu'
     assert attack_wrapper.norm == 'Linf'
     assert attack_wrapper.version == 'standard'
+
+    epsilon = 0.123
+    out = attack_wrapper.execute(model, data.squeeze(0), target, epsilon, verbose_value) 
+
+    attack_wrapper.verbose = verbose_value
+
+    assert hasattr(attack_wrapper, "verbose")
+    assert attack_wrapper.verbose == verbose_value
+
+    assert isinstance(out, torch.Tensor)
+    assert out.device.type == "cpu"
+
+
