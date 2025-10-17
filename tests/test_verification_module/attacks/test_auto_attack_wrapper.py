6ff6c9fd203c19b75dc4d5f54bbdcd08463a8877
AWbosman <annelotbosman@gmail.com>
2025-10-17 17:49:16 +0200

    minor changes

--- tests/test_verification_module/attacks/test_auto_attack_wrapper.py
+++ tests/test_verification_module/attacks/test_auto_attack_wrapper.py
@@ -1,8 +1,36 @@
+import pytest
+from pyautoattack import AutoAttack
 
 
+class DummyModel:
+    def __call__(self, x):
+        return x
 
+@pytest.fixture
+def dummy_model():
+    return DummyModel()
+
+def test_verbose_assignment(dummy_model):
+    epsilon = 0.1
+    norm = 'Linf'
+    version = 'standard'
+    verbose_value = False  
+
+    adversary = AutoAttack(
+        dummy_model,
+        norm=norm,
+        eps=epsilon,
+        version=version,
+        device='cpu',
+    )
+
+    adversary.verbose = verbose_value
+
+    assert hasattr(adversary, 'verbose'), "AutoAttack should have a 'verbose' attribute"
+    assert adversary.verbose == verbose_value, "Verbose flag not correctly set"
+    
+    
 def test_auto_attack_wrapper_initialization(attack_wrapper):
     assert attack_wrapper.device == 'cpu'
     assert attack_wrapper.norm == 'Linf'
     assert attack_wrapper.version == 'standard'
-    assert not attack_wrapper.verbose
