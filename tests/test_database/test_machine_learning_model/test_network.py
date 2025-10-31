75105ebb3ad016efece01dd47928722dddfed09e
AaronB99 <aaronberger@hotmail.de>
2025-10-31 13:16:23 +0100

    increased test coverage

--- tests/test_database/test_machine_learning_model/test_network.py
+++ tests/test_database/test_machine_learning_model/test_network.py
@@ -26,4 +26,34 @@ def test_abstract_methods_raise_not_implemented_error(network):
         Network.to_dict(network)
 
     with pytest.raises(NotImplementedError):
-        Network.from_file(dict(name="network",path=Path("test")))
+        Network.from_file(dict(name="network", path=Path("test")))
+
+    class ConcreteNetwork(Network):
+        def load_pytorch_model(self, *args, **kwargs):
+            return super().load_pytorch_model(*args, **kwargs)
+
+        def get_input_shape(self, *args, **kwargs):
+            return super().get_input_shape(*args, **kwargs)
+
+        def to_dict(self, *args, **kwargs):
+            return super().to_dict(*args, **kwargs)
+
+        @classmethod
+        def from_dict(cls, data: dict):
+            return super().from_dict(data)
+
+        @property
+        def name(self):
+            return super().name
+
+        @property
+        def path(self):
+            return super().path
+
+    inst = ConcreteNetwork()
+
+    with pytest.raises(NotImplementedError):
+        _ = inst.name
+
+    with pytest.raises(NotImplementedError):
+        _ = inst.path
