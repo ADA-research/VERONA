417538da28a8de4aa5fdf751a96a4898af59622b
Annelot Bosman <75082965+AWbosman@users.noreply.github.com>
2025-11-07 11:38:02 +0000

    flatten() gave issue with  multiple labels (#154)
    * flatten() gave issue with  multiple labels
    
    * removed numel>0 part
    
    * ruf fix

--- ada_verona/database/epsilon_status.py
+++ ada_verona/database/epsilon_status.py
@@ -35,4 +35,5 @@ class EpsilonStatus:
             dict: The dictionary representation of the EpsilonStatus.
         """
         return dict(epsilon_value=self.value, result=self.result, time=self.time, verifier=self.verifier, 
-                    obtained_labels=self.obtained_labels.flatten().tolist() if self.obtained_labels else None)
+                    obtained_labels=self.obtained_labels.flatten().tolist() if self.obtained_labels is not None 
+                    else None)
