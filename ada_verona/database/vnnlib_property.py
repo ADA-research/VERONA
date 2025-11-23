846726844f582c407cdf8fbad3c39da1d5cad0a1
Hendrik <hendrik@hendrikusb.com>
2025-11-23 17:50:23 +0100

    Add image and image_class to vnnlib_property

--- ada_verona/database/vnnlib_property.py
+++ ada_verona/database/vnnlib_property.py
@@ -15,6 +15,7 @@
 
 from dataclasses import dataclass
 from pathlib import Path
+import numpy as np
 
 
 @dataclass
@@ -24,3 +25,6 @@ class VNNLibProperty:
     name: str
     content: str
     path: Path = None
+    epsilon: float | None = None
+    image: np.ndarray | None = None
+    image_class: int | None = None
